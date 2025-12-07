97e7338b8cc9fc02295ab468bf85e253e752e796
SriHariniPro <143264370+SriHariniPro@users.noreply.github.com>
2025-12-07 17:26:12 +0530

    Refactor longestConsecutive method for clarity

--- 128. Longest Consecutive Sequence.java
+++ 128. Longest Consecutive Sequence.java
@@ -1,20 +1,43 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-      Set<Integer> s = new HashSet<>();
-      for(int n:nums){
-        s.add(n);
-      }
-      int ml=0;
-      for(int n:s){
-        if(!s.contains(n-1)){
-            int l =1;
-            while(s.contains(n+l)){
-                l++;
-            }
-        ml = Math.max(ml,l); 
+        Set<Integer> se = new HashSet<>();
+        for(int n:nums){
+            se.add(n);
         }
-      }
-      return ml;
-      
+        int ml=0;
+        Map<Integer,Integer> m = new HashMap<>();
+        for(int n:nums){
+            int cn=n;
+            while(se.contains(cn)){
+                se.remove(cn);
+                cn++;
+            }
+            int sl = cn-n + m.getOrDefault(cn,0);
+            m.put(n,sl);
+            ml = Math.max(ml,m.get(n));
+        }
+        return ml;
+    }
+}
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> se = new HashSet<>();
+        for(int n:nums){
+            se.add(n);
+        }
+        int ml=0;
+        for(int n:se){
+            int cn = n;
+            int c=1;
+            if(!se.contains(cn-1)){
+                while(se.contains(cn+1)){
+                    c++;
+                    cn++;
+                }
+            }
+            ml = Math.max(ml,c);
+        }
+        return ml;
     }
 }
